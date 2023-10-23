f0cc75bcc7a3c2d7051d8185f477dd20ae5f4710
Npmauney <nathanielmauney@gmail.com>
2023-10-23 18:18:11 -0400

    finished solutions

--- solution5.py
+++ solution5.py
@@ -1,1 +1,9 @@
 # Write code for algorithm 5 below
+def is_palindrome(s):
+    s=s.replace(" ", "").lower()
+
+    return s == s[::-1]
+
+print(is_palindrome("racecar"))
+print(is_palindrome("hello"))
+print(is_palindrome("A man a plan a canal Panama"))
